8c134b2f946032d16694311b6c5b32331e8d01b9
Liam Bagabag <lbscout1@gmail.com>
2025-01-24 14:41:55 -0800

    xor model cont

--- src/nn.c
+++ src/nn.c
@@ -10,47 +10,91 @@
 
 static RandomSeries local_series;
 
+typedef struct {
+    Matrix a0;
+    Matrix w1, b1, a1;
+    Matrix w2, b2, a2;
+} Xor;
+
+float forward_xor(Xor m)
+{
+    MatrixDot(m.a1, m.a0, m.w1);
+    MatrixSum(m.a1, m.b1);
+    MatrixSigmoid(m.a1);
+
+    MatrixDot(m.a2, m.a1, m.w2);
+    MatrixSum(m.a2, m.b2);
+    MatrixSigmoid(m.a2);
+
+    return *m.a2.V;
+}
+
+float cost(Xor m, Matrix ti, Matrix to)
+{
+    Assert(ti.rows == to.rows, "");
+    Assert(to.cols == m.a2.cols, "");
+    size_t n = ti.rows;
+
+    float c = 0;
+    for (size_t i = 0; i < n; i++) {
+        Matrix x = MatrixRow(ti, i);
+        Matrix y = MatrixRow(to, i);
+
+        MatrixCopy(m.a0, x);
+        forward_xor(m);
+
+        size_t q = to.cols;
+        for (size_t j = 0; j < q; j++) {
+            float d = MatrixAT(m.a2, 0, j) - MatrixAT(y, 0, j);
+            c += d * d;
+        }
+
+    }
+    return c/n;
+}
+
+float ti[] = {
+    0, 0, 0,
+    0, 1, 1,
+    1, 0, 1,
+    1, 1, 0
+};
+
 int main(void)
 {
     Arena* local_arena = ArenaMalloc(Kilobytes(2));
     local_series = RandomSeed(122);
 
-    Matrix x  = MatrixAlloc(local_arena, 1, 2);
-    Matrix w1 = MatrixAlloc(local_arena, 2, 2);
-    Matrix b1 = MatrixAlloc(local_arena, 1, 2);
-    Matrix a1 = MatrixAlloc(local_arena, 1, 2); // activation
+    Xor m;
 
-    Matrix w2 = MatrixAlloc(local_arena, 2, 1);
-    Matrix b2 = MatrixAlloc(local_arena, 1, 1);
-    Matrix a2 = MatrixAlloc(local_arena, 1, 1);
 
-    MatrixRandomize(&local_series, w1, 0, 1);
-    MatrixRandomize(&local_series, w2, 0, 1);
-    MatrixRandomize(&local_series, b1, 0, 1);
-    MatrixRandomize(&local_series, b2, 0, 1);
+    m.a0 = MatrixAlloc(local_arena, 1, 2);
 
-    float x1 = 0;
-    float x2 = 1;
-    MatrixAT(x, 0, 0) = x1;
-    MatrixAT(x, 0, 1) = x2;
+    m.w1 = MatrixAlloc(local_arena, 2, 2);
+    m.b1 = MatrixAlloc(local_arena, 1, 2);
+    m.a1 = MatrixAlloc(local_arena, 1, 2); // activation
 
-    MatrixDot(a1, x, w1);
-    MatrixSum(a1, b1);
-    MatrixSigmoid(a1);
+    m.w2 = MatrixAlloc(local_arena, 2, 1);
+    m.b2 = MatrixAlloc(local_arena, 1, 1);
+    m.a2 = MatrixAlloc(local_arena, 1, 1);
 
-    MatrixDot(a2, a1, w2);
-    MatrixSum(a2, b2);
-    MatrixSigmoid(a2);
+    MatrixRandomize(&local_series, m.w1, 0, 1);
+    MatrixRandomize(&local_series, m.w2, 0, 1);
+    MatrixRandomize(&local_series, m.b1, 0, 1);
+    MatrixRandomize(&local_series, m.b2, 0, 1);
 
-    float y = *a2.V;
+    printf("cost = %f\n", cost(m, ti, to));
 
-    MatrixPrint(x);
-    MatrixPrint(a1);
-    MatrixPrint(w1);
-    MatrixPrint(b1);
-    MatrixPrint(w2);
-    MatrixPrint(b2);
-    MatrixPrint(a2);
+    for (size_t i = 0; i < 2; i++) {
+        for (size_t j = 0; j < 2; j++) {
+            MatrixAT(m.a0, 0, 0) = i;
+            MatrixAT(m.a0, 0, 1) = j;
+            forward_xor(m);
+            float y = *m.a2.V;
+
+            printf("%zu ^ %zu = %f\n", i, j, y);
+        }
+    }
 
     ArenaFree(local_arena);
     return 0;
