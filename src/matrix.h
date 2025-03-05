c03322c6cb9461e1a0e202d562195198b9fbb3ae
Liam Bagabag <lbscout1@gmail.com>
2025-03-05 12:11:15 -0800

    starting on learning process

--- src/matrix.h
+++ src/matrix.h
@@ -38,18 +38,18 @@ typedef struct Matrix {
 #define MatrixAT(m, i, j) ((m).V[(i) * m.cols + (j)])
 #define RowAT(r, i) MatrixAT(r, 0, i)
 
-Matrix MatrixAlloc(size_t rows, size_t cols, float*);
+Matrix MatrixAlloc(size_t, size_t, float*);
 
 #define MatrixArenaAlloc(arena, i, j) (MatrixAlloc((i), (j), PushArray(arena, float, (i) * (j))))
 #define RowArenaAlloc(arena, i) MatrixArenaAlloc(arena, 1, i)
 
-Matrix MatrixMalloc(size_t rows, size_t cols);
+Matrix MatrixMalloc(size_t, size_t);
 void MatrixFree(Matrix a);
 
-void MatrixPrint_(Matrix a, const char* name);
+void MatrixPrint_(Matrix a, const char*);
 #define MatrixPrint(m) MatrixPrint_(m, #m)
 
-void MatrixRandomize(RandomSeries* series, Matrix a, float low, float high);
+void MatrixRandomize(RandomSeries *, Matrix, float, float);
 void MatrixFill(Matrix, float);
 
 Matrix MatrixRow(Matrix, size_t);
@@ -63,6 +63,15 @@ void MatrixDot(Matrix, Matrix, Matrix);
 
 void MatrixSum(Matrix, Matrix);
 void MatrixTranspose(Matrix, Matrix);
+
+void MatrixShuffleValue(RandomSeries *, Matrix);
+
+bool32 MatrixRandomShuffleRow(RandomSeries *, Matrix, size_t, size_t *);
+bool32 MatrixRandomShuffleCol(RandomSeries *, Matrix, size_t, size_t *);
+bool32 MatrixShuffleRow(Matrix, size_t *, size_t);
+bool32 MatrixShuffleCol(Matrix, size_t *, size_t);
+#define RowRandomShuffle MatrixRandomShuffleCol
+#define RowShuffle MatrixShuffleCol
 
 void MatrixApply(Matrix, float (*)(float));
 void MatrixSigmoid(Matrix); // "apply" wrapper with sigmoidf
@@ -291,7 +300,6 @@ MatrixTranspose(Matrix b, Matrix a)
             /*b.V[i * b.cols + j] = a.V[j * a.cols + i];*/
             MatrixAT(b, i, j) = MatrixAT(a, j, i);
         }
-        printf("\n");
     }
 }
 
@@ -310,6 +318,162 @@ void
 MatrixReLU(Matrix a)
 {
     MatrixApply(a, reluf);
+}
+
+void
+MatrixShuffleValue(RandomSeries *series, Matrix a)
+{
+    for (size_t i = 0; i < a.rows; i++) {
+        for (size_t j = 0; j < a.cols; j++) {
+            uint32 row = RandomChoice(series, a.rows);
+            uint32 col = RandomChoice(series, a.cols);
+
+            float32 tmp = MatrixAT(a, i, j);
+            MatrixAT(a, i, j) = MatrixAT(a, row, col);
+            MatrixAT(a, row, col) = tmp;
+        }
+    }
+}
+
+bool32
+MatrixRandomShuffleRow(RandomSeries * series, Matrix a, size_t count_per_pair, size_t *shuffle_indices)
+{
+    // NOTE(liam) shuffle_indices' size must be at least a.cols
+    bool32 res = true;
+    if (series == NULL)
+    {
+        res = false;
+    }
+    else
+    {
+        size_t *pos = shuffle_indices;
+
+        while (count_per_pair--)
+        {
+            size_t rowA = RandomChoice(series, a.rows);
+            size_t rowB = RandomChoice(series, a.rows);
+
+            if (rowA == rowB) continue;
+
+            if (pos)
+            {
+                *(pos++) = rowA;
+                *(pos++) = rowB;
+            }
+
+            for (size_t j = 0; j < a.cols; j++)
+            {
+                float32 tmp = MatrixAT(a, rowA, j);
+                MatrixAT(a, rowA, j) = MatrixAT(a, rowB, j);
+                MatrixAT(a, rowB, j) = tmp;
+            }
+        }
+    }
+    return res;
+}
+
+bool32
+MatrixRandomShuffleCol(RandomSeries * series, Matrix a, size_t count_per_pair, size_t *shuffle_indices)
+{
+    // NOTE(liam): shuffle_indices can be nullable if user does not need to
+    // know the shuffle indices. indices are inserted 2 per count_per_pair: (from, to)
+    // therefore assume the following: shuffle_indices[count_per_pair * 2]
+    bool32 res = true;
+    if (series == NULL)
+    {
+        res = false;
+    }
+    else
+    {
+        size_t *pos = shuffle_indices;
+
+        while (count_per_pair--)
+        {
+            size_t colA = RandomChoice(series, a.cols);
+            size_t colB = RandomChoice(series, a.cols);
+
+            if (colA == colB) continue;
+
+            if (pos)
+            {
+                *(pos++) = colA;
+                *(pos++) = colB;
+            }
+
+            for (size_t i = 0; i < a.rows; i++)
+            {
+                float32 tmp = MatrixAT(a, i, colA);
+                MatrixAT(a, i, colA) = MatrixAT(a, i, colB);
+                MatrixAT(a, i, colB) = tmp;
+            }
+        }
+    }
+    return res;
+}
+
+bool32
+MatrixShuffleRow(Matrix a, size_t *shuffle_indices, size_t count_per_pair)
+{
+    // NOTE(liam): count_per_pair == sizeof(shuffle_indices) / 2
+    bool32 res = true;
+    if (shuffle_indices == NULL)
+    {
+        res = false;
+    }
+    else
+    {
+        size_t pos = 0;
+        while (count_per_pair-- > 0)
+        {
+            // TODO(liam): i feel like this operation is not efficiently indexed.
+            size_t rowA = shuffle_indices[pos++];
+            size_t rowB = shuffle_indices[pos++];
+
+            Assert(rowA < a.rows);
+            Assert(rowB < a.rows);
+            if (rowA == rowB) continue;
+
+            for (size_t j = 0; j < a.cols; j++)
+            {
+                float32 tmp = MatrixAT(a, rowA, j);
+                MatrixAT(a, rowA, j) = MatrixAT(a, rowB, j);
+                MatrixAT(a, rowB, j) = tmp;
+            }
+        }
+    }
+    return res;
+}
+
+bool32
+MatrixShuffleCol(Matrix a, size_t *shuffle_indices, size_t count_per_pair)
+{
+    bool32 res = true;
+    if (shuffle_indices == NULL)
+    {
+        res = false;
+    }
+    else
+    {
+        size_t pos = 0;
+        while (count_per_pair-- > 0)
+        {
+            size_t colA = shuffle_indices[pos++];
+            size_t colB = shuffle_indices[pos++];
+            /*printf("DEBUG: shuffling col %lu -> %lu.\n", colA, colB);*/
+
+            Assert(colA < a.cols);
+            Assert(colB < a.cols);
+            if (colA == colB) continue;
+
+            for (size_t i = 0; i < a.rows; i++)
+            {
+                float32 tmp = MatrixAT(a, i, colA);
+                MatrixAT(a, i, colA) = MatrixAT(a, i, colB);
+                MatrixAT(a, i, colB) = tmp;
+            }
+        }
+    }
+    return res;
 }
 
 #endif
--- tests/network.c
+++ tests/network.c
@@ -15,6 +15,13 @@ typedef struct NeuralNet {
     Row *B;
 } NeuralNet;
 
+// NOTE(liam): this will only exist inside functions pertaining to the
+// NeuralNet struct, so the size will always be derived from there.
+typedef struct NeuralHelper {
+    Row *Z;
+    Row *A;
+} NeuralHelper;
+
 void NeuralNetInit(Arena* arena, RandomSeries *series, NeuralNet* nn, size_t *layerSizes, size_t layerCount)
 {
     nn->layerCount = layerCount; // total # of layers
@@ -42,93 +49,117 @@ void NeuralNetInit(Arena* arena, RandomSeries *series, NeuralNet* nn, size_t *la
     }
 }
 
-void NeuralNetForward(Arena *arena, NeuralNet nn, Row x)
+void NeuralHelperInit(Arena *arena, NeuralHelper *nh, NeuralNet nn)
 {
-    // NOTE(liam): a[l] = w * a[l-1] + b; a[0] = x
-    ArenaTemp tmp = ArenaScratchCreate(arena);
+    nh->Z = PushArray(arena, Row, nn.layerCount - 1);
+    nh->A = PushArray(arena, Row, nn.layerCount - 1);
 
-    Matrix Z[nn.layerCount];
-    Matrix A[nn.layerCount];
     for (uint32 l = 0; l < nn.layerCount - 1; l++)
     {
-        Z[l] = RowArenaAlloc(arena, nn.layerSizes[l + 1]);
-        A[l] = RowArenaAlloc(arena, nn.layerSizes[l + 1]);
+        nh->Z[l] = RowArenaAlloc(arena, nn.layerSizes[l + 1]);
+        nh->A[l] = RowArenaAlloc(arena, nn.layerSizes[l + 1]);
     }
+}
 
+void NeuralNetForward(NeuralHelper *nh, NeuralNet nn, Row x)
+{
+    // NOTE(liam): a[l] = w * a[l-1] + b; a[0] = x
+    Row *Z = nh->Z;
+    Row *A = nh->A;
+
+    // NOTE(liam): a[1] = z; z = w * a[0] + b
     MatrixDot(Z[0], x, nn.W[0]);
-
-    printf("failing sizes: Z.col = %lu, W.col = %lu\n", Z->cols, nn.W->cols);
-    /*MatrixAddMatrix(Z[0], Z[0], nn.B[0]);*/
-    /*MatrixCopy(A[0], Z[0]);*/
-    /*MatrixSigmoid(A[0]);*/
+    MatrixAddMatrix(Z[0], Z[0], nn.B[0]);
+    MatrixCopy(A[0], Z[0]);
+    MatrixSigmoid(A[0]);
 
     for (uint32 l = 1; l < nn.layerCount - 1; l++)
     {
-        /*MatrixDot(Z[l], A[l-1], nn.W[l]);*/
-        /*MatrixAddMatrix(Z[l], Z[l], nn.B[l]);*/
-        /*MatrixCopy(A[l], Z[l]);*/
-        /*MatrixSigmoid(A[l]);*/
+        MatrixDot(Z[l], A[l-1], nn.W[l]);
+        MatrixAddMatrix(Z[l], Z[l], nn.B[l]);
+        MatrixCopy(A[l], Z[l]);
+        MatrixSigmoid(A[l]);
     }
-
-    for (uint32 l = 0; l < nn.layerCount; l++)
-    {
-        MatrixPrint(A[l]);
-    }
-
-    ArenaScratchFree(tmp);
 }
 
 float32 NeuralNetCost(NeuralNet, Matrix);
 
-void NeuralNetBackprop(NeuralNet);
-void NeuralNetLearn(NeuralNet);
+void NeuralNetBackprop(Arena *arena, NeuralNet nn, Row x)
+{
+    NeuralHelper nh = {0};
+    NeuralHelperInit(arena, &nh, nn);
+
+    NeuralNetForward(&nh, nn, x); // populates nh with A and Z
+
+}
+
+void NeuralNetLearn(RandomSeries *series,
+        NeuralNet nn, Matrix x_train, Row y_train,
+        size_t epochs, size_t batch_size)
+{
+    size_t n = x_train.rows * x_train.cols;
+    for (size_t i = 0; i < epochs; i++)
+    {
+        size_t shuffleCount = x_train.cols;
+        size_t swapIdx[shuffleCount * 2];
+
+        MatrixRandomShuffleRow(series, x_train, shuffleCount, swapIdx);
+
+        /*printf("values of swapIdx\n");*/
+        /*for (int j = 0; j < x_train.cols * 2; j++)*/
+        /*{*/
+        /*    printf("%d\n", swapIdx[j]);*/
+        /*}*/
+
+        MatrixShuffleCol(y_train, swapIdx, shuffleCount);
+
+        // TODO(liam): continue here for learning algo
+    }
+
+    MatrixPrint(x_train);
+    MatrixPrint(y_train);
+}
 
 int main(void)
 {
     Arena arena = {0};
     RandomSeries series = {0};
-    RandomSeed(&series, 64);
+    RandomSeed(&series, 123423213);
 
     size_t layerCount = 12;
-    size_t sizes[3] = {3, 8, 4};
+    size_t sizes[] = {2, 8, 6, 4, 1};
     NeuralNet nn = {0};
-    NeuralNetInit(&arena, &series, &nn, sizes, 3);
+    NeuralNetInit(&arena, &series, &nn, sizes, ArrayCount(sizes));
 
-    for (int i = 0; i < 2; i++)
-    {
-        MatrixPrint(nn.W[i]);
-        MatrixPrint(nn.B[i]);
-    }
+    /*for (int i = 0; i < ArrayCount(sizes) - 1; i++)*/
+    /*{*/
+        /*MatrixPrint(nn.W[i]);*/
+        /*MatrixPrint(nn.B[i]);*/
+    /*}*/
 
-    Row x_train = RowArenaAlloc(&arena, 3);
-    RowAT(x_train, 0) = 1;
-    RowAT(x_train, 1) = 2;
-    RowAT(x_train, 2) = 3;
+    // TODO(liam): super inefficient assignments, might rework later
+    Matrix x_train = MatrixArenaAlloc(&arena, 4, 2);
+    MatrixAT(x_train, 0, 0) = 0;
+    MatrixAT(x_train, 0, 1) = 0;
+    MatrixAT(x_train, 1, 0) = 0;
+    MatrixAT(x_train, 1, 1) = 1;
+    MatrixAT(x_train, 2, 0) = 1;
+    MatrixAT(x_train, 2, 1) = 0;
+    MatrixAT(x_train, 3, 0) = 1;
+    MatrixAT(x_train, 3, 1) = 1;
+
+    Row y_train = RowArenaAlloc(&arena, 4);
+    RowAT(y_train, 0) = 0;
+    RowAT(y_train, 1) = 1;
+    RowAT(y_train, 2) = 1;
+    RowAT(y_train, 3) = 1;
+    /*RowAT(x_train, 2) = 3;*/
     MatrixPrint(x_train);
+    MatrixPrint(y_train);
 
-    NeuralNetForward(&arena, nn, x_train);
+    /*NeuralNetBackprop(&arena, nn, x_train);*/
 
-    /*Matrix Z0 = MatrixArenaAlloc(&arena, 1, 8);*/
-    /*Matrix A0 = MatrixArenaAlloc(&arena, 1, 8);*/
-    /**/
-    /*MatrixDot(Z0, x_train, nn.W[0]); // 1x8 = 1x3 * 3x8*/
-    /*MatrixAddMatrix(Z0, Z0, nn.B[0]); // 1x8 = 1x8 + 1x8*/
-    /*MatrixCopy(A0, Z0);*/
-    /*MatrixSigmoid(A0);*/
-    /**/
-    /*MatrixPrint(Z0);*/
-    /*MatrixPrint(A0);*/
-    /**/
-    /*Matrix Z1 = MatrixArenaAlloc(&arena, 1, 4); // (1x8 * 8x4) + 1x4*/
-    /*Matrix A1 = MatrixArenaAlloc(&arena, 1, 4);*/
-    /**/
-    /*MatrixDot(Z1, A0, nn.W[1]); // 1x8 = 1x3 * 3x8*/
-    /*MatrixAddMatrix(Z1, Z1, nn.B[1]); // 1x8 = 1x8 + 1x8*/
-    /*MatrixCopy(A1, Z1);*/
-    /*MatrixSigmoid(A1);*/
-    /**/
-    /*MatrixPrint(Z1);*/
-    /*MatrixPrint(A1);*/
+    NeuralNetLearn(&series, nn, x_train, y_train, 1, 1);
 
     ArenaFree(&arena);
     return 0;
